e1ae628a58aedbffde21709c114cda205c2490a8
JoseTaquiriVicerrel <vicerrel15@gmail.com>
2022-08-21 20:45:44 +0000

    Initial commit

---
+++ init.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader = " "
+vim.o.number = true                                                                   
+vim.o.relativenumber = true 
+vim.o.expandtab = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.smartindent = 4
+vim.g.clipbloard = "unnamedplus"
+vim.cmd("colorscheme nordfox")
+
+require "plugins"
+
---
+++ lua/plugins.lua
@@ -0,0 +1,52 @@
+vim.cmd([[
+	  augroup packer_user_config
+	    autocmd!
+	    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+	  augroup end
+	]])
+
+
+vim.cmd [[packadd packer.nvim]]
+
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end
+    }
+    use 'nvim-treesitter/playground'
+    use {
+        "williamboman/nvim-lsp-installer",
+        "neovim/nvim-lspconfig",
+    }
+    use {
+        'hrsh7th/nvim-cmp',
+        requires = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+        }
+    }
+    use {
+        'lewis6991/gitsigns.nvim',
+        -- tag = 'release' -- To use the latest release
+    }
+    use "EdenEast/nightfox.nvim" -- Packer
+    use "tpope/vim-fugitive"
+    use "tpope/vim-surround"
+    use "tpope/vim-commentary"
+    use 'jwalton512/vim-blade'
+    use 'feline-nvim/feline.nvim'
+
+    use 'kyazdani42/nvim-web-devicons'
+end
+)
